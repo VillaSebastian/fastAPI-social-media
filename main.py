7618f31b601082957b40d11830866f64f13208e7
Sebastian Villa <sebastian.villal@outlook.com>
2024-05-10 23:44:21 -0500

    Added an html scaffolding for the project and a way to serve static files using FileResponse

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse
 
 app = FastAPI()
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return FileResponse('./static/index.html')
