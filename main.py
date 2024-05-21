e9877c48c20a1c6126e0d2ea107a095c1ab2b3e9
Sebastian Villa <sebastian.villal@outlook.com>
2024-05-21 12:43:14 -0500

    Implement database integration for all API endpoints

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.staticfiles import StaticFiles
-from starlette.responses import FileResponse
+from fastapi.responses import FileResponse
 from pydantic import BaseModel
-from random import randint
 import sqlite3
 
 
@@ -12,10 +11,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Connect to the database (or create it if it doesn't exits)
 conn = sqlite3.connect('social_media.db')
-
-# Create a cursos object to execute SQL queries
 cursor = conn.cursor()
-    
 # Create a table posts (if it doesn't exist)
 cursor.execute('''CREATE TABLE IF NOT EXISTS posts (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,72 +19,87 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS posts (
                content TEXT NOT NULL,
                created_at TEXT DEFAULT current_timestamp
                 )''')
-
-# Insert data into the table
-# cursor.execute("INSERT INTO posts (title, content) VALUES (?, ?)", ('This is my first post', 'some interesting content'))
-
-# Commit the changes
+# Commit and close the connection
 conn.commit()
-
-# Close the cursos and connection
 cursor.close()
-# conn.close()
+conn.close()
 
 
 class Post(BaseModel):
     title: str
     content: str
 
-posts_examples = [{'title': 'This is my first post', 'content': 'some interesting content','id': 1}, {'title': 'A clickbait title', 'content': 'Generic content','id': 2}]
-
-def find_post(id):
-    for post in posts_examples:
-        if post['id'] == id:
-            return post
 
 @app.get("/")
 def root():
+    """Serve the index.html file."""
     return FileResponse('./static/index.html')
+
 
 @app.get("/posts")
 def get_posts():
-    # Fetch and print data from the table
+    """Fetch and return all posts."""
     conn = sqlite3.connect('social_media.db')
     cursor = conn.cursor()
     rows = cursor.execute("SELECT * FROM posts").fetchall()
-    print(rows)
     posts = [{"id": row[0], "title": row[1], "content": row[2], "created_at": row[3]} for row in rows]
     cursor.close()
     conn.close()
     return {'data': posts}
 
+
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    post = post.dict()
-    post['id'] = randint(1, 1000000)
-    posts_examples.append(post)
-    return {'post created:': f'title {post["title"]} content: {post["content"]}'}
+    """Create a new post."""
+    conn = sqlite3.connect('social_media.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO posts (title, content) VALUES (?, ?)", (post.title, post.content))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {'Message': 'Post created successfully'}
+
 
 @app.get("/posts/{id}")
 def get_one_post(id: int, response: Response):
-    post = find_post(id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
-        detail=f'post with id {id} was not found')  
+    """Fetch and return a post by ID."""
+    conn = sqlite3.connect('social_media.db')
+    cursor = conn.cursor()
+    print('The id is', id, 'and the type is', type(id))
+    row = cursor.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    if row is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The post {id} was not found')
+    post = {"id": row[0], "title": row[1], "content": row[2], "created_at": row[3]}
+    cursor.close()
+    conn.close()
     return {"post_detail": post}
+
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    post = find_post(id)
-    if not post:
+    """Delete a post by ID."""
+    conn = sqlite3.connect('social_media.db')
+    cursor = conn.cursor()
+    row = cursor.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    if row is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The post {id} was not found')
-    posts_examples.remove(post)
+    row = cursor.execute('DELETE FROM posts WHERE id = ?', (id,)).fetchone()
+    conn.commit()
+    cursor.close()
+    conn.close()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
 
 @app.patch("/posts/{id}")
 def update_post(id: int, updated_post: Post):
-    for post in posts_examples:
-        if post['id'] == id:
-            post['title'] = updated_post.title
-            post['content'] = updated_post.content
-    return {"Message": "Post updated succesfully"}
+    """Update a post by ID."""
+    conn = sqlite3.connect('social_media.db')
+    cursor = conn.cursor()
+    row = cursor.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    if row is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'The post {id} was not found')
+    cursor.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (updated_post.title, updated_post.content, id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return {"Message": "Post updated successfully"}
